3c680b028b740a6a0c195cee132b906a97506b0a
Lucas Carvalho Simões <lucas.simoes@al.infnet.edu.br>
2024-03-25 11:03:52 -0300

    remove localStorage error

--- components/MainContainer/MainContainer.tsx
+++ components/MainContainer/MainContainer.tsx
@@ -5,25 +5,25 @@ import styles from './style.module.css'
 
 const MainContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
-    function getWithExpiry(key: string) {
-        const itemStr = localStorage.getItem(key);
-        if (!itemStr) {
-            return null;
-        }
-        const item = JSON.parse(itemStr);
-        const now = new Date();
-        if (now.getTime() > item.expiry) {
-            localStorage.removeItem(key);
-            return null;
-        }
-        return item.value;
-    }
+    // function getWithExpiry(key: string) {
+    //     const itemStr = localStorage.getItem(key);
+    //     if (!itemStr) {
+    //         return null;
+    //     }
+    //     const item = JSON.parse(itemStr);
+    //     const now = new Date();
+    //     if (now.getTime() > item.expiry) {
+    //         localStorage.removeItem(key);
+    //         return null;
+    //     }
+    //     return item.value;
+    // }
 
-    const valor = getWithExpiry("chave") == null ? getWithExpiry("chave") : sessionStorage.getItem('chave');
+    // const valor = getWithExpiry("chave") == null ? getWithExpiry("chave") : sessionStorage.getItem('chave');
 
     return (
         <main className={styles.container}>
-            <Header auth={valor}/>
+            <Header auth={"valor"}/>
             <div> { children } </div>
         </main>
     )
